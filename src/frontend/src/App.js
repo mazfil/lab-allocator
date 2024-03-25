e2f6cafca6409ec488a6fab19eea509472d80cef
Filip <hey@fmaz.me>
2024-03-25 14:25:22 +1100

    Front end dashboard design implemented

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+/*import logo from './ANU_Primary_Horizontal_GoldBlack.svg';*/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <div className='body'>
+        <div className='main-logo'>
+  {/*
+          <img src={logo} alt='ANU Logo with Gold Crest and the words Australian National University '></img>
+          REMOVE PLACEHOLDER UPON LOGO APPROVAL FROM BRANDING
+  */}
+          <div className='LOGO-PLACEHOLDER'>
+            <i class="bi bi-shield"></i>
+            <h1>LOGO PENDING APPROVAL</h1>
+          </div>
+          <h1>SoCo Lab Allocation System</h1>
+        </div>
+        <div className='line-break'>
+        </div>
+        <div className='controls'>
+          <div className='main-controls'>
+            <div className='data-mgmt'>
+              <div id='upload-data'>
+                <button type="button">Upload Data</button>
+              </div>
+              <div>
+                <button type="button">Manage Data</button>
+              </div>
+            </div>
+            <div className='timetable-mgmt'>
+              <button type="button">Manage Timetable</button> 
+            </div>
+          </div>
+          <div className='control-gap'></div>
+          <div className='app-mgmt'>
+            <button type="button">About</button>
+            <button type="button" id='help-supp' >Help & Support</button>
+            <button type="button">Status</button>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
