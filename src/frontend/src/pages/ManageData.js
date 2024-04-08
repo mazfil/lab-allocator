32f6abef8dd6f31a8f5a5b34191dfb4ab47cb6fd
Filip <hey@fmaz.me>
2024-04-08 17:58:12 +1000

    Implemented firebase connection and basic table

--- src/frontend/src/pages/ManageData.js
+++ src/frontend/src/pages/ManageData.js
@@ -1,15 +1,62 @@
-import {Component, useState} from 'react';
 import NavBar from '../components/nav/NavBar';
+import { useState } from 'react';
+import {collection, getDocs} from 'firebase/firestore'
+import {db} from '../firebase';
+import { useEffect } from 'react';
+function ManageData(props){
 
-export default class ManageData extends Component{
-    constructor(props){
-        super(props)
-    }
+    const [course_data, setCourseData] = useState([]);
+
     
-    render(){
-        return(
-            <NavBar navigate={this.props.navigate} tab={'manage-data'}></NavBar>
-            // TODO: Code for Data Management Page
-        )
+
+    const fetchPost = async () => {
+        await getDocs(collection(db, "course_data"))
+            .then((querySnapshot)=>{               
+                const newData = querySnapshot.docs.map((doc) => ({...doc.data(), key: doc.id}));
+                setCourseData(newData);
+            })
     }
+
+    
+
+
+    useEffect(() => {fetchPost();}, [])
+    let x = {}
+    course_data.forEach(c => {
+        
+    });
+    
+
+    return(
+        <div className='data-table'>
+            <NavBar navigate={props.navigate} tab={'manage-data'}></NavBar>
+            <div className='data-table'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Course Code</th>
+                        <th>Course Size</th>
+                        <th>Lectures</th>
+                        <th>Lab Preferences</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {course_data.map((course) => {
+                        return(
+                            <tr>
+                                <td>{course.id}</td>
+                                <td>{course.course_size}</td>
+                                <td>asd</td>
+                                <td>asd</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            </div>
+            
+        </div>
+
+    )
 }
+export default ManageData;
