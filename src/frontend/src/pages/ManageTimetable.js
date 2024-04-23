8cd3e3b14c385c2f755b22e3287bc11aa6a5e046
Filip <hey@fmaz.me>
2024-04-23 14:02:47 +1000

    Fetching tutorial data from collections

--- src/frontend/src/pages/ManageTimetable.js
+++ src/frontend/src/pages/ManageTimetable.js
@@ -1,15 +1,30 @@
 import {Component} from 'react';
 import NavBar from '../components/nav/NavBar';
+import { useState } from 'react';
+import {collection, getDocs, get} from 'firebase/firestore'
+import { useEffect } from 'react';
+import * as helpers from "../utils/helperFunctions.js";
 
-export default class ManageTimetable extends Component{
-    constructor(props){
-        super(props)
+function ManageTimetable(props){
+    const [timetable_data, setTimetableData] = useState([]);
+    const [tutorials, setTutData] = useState([]);
+
+
+    const fetchPost = async () => {
+        const timetables = await helpers.getData("timetable");
+        setTimetableData(timetables)
+        const tutorials = await helpers.getData("timetable/"+timetables[0].id+"/tutorials");
+        setTutData(tutorials)
     }
-    render(){
-        return(
-            <NavBar navigate={this.props.navigate} tab={'manage-timetable'}></NavBar>
-            // TODO: Code for Timetable Management Page
-        )
-    }
+
+
+    useEffect(() => {fetchPost();}, [])
+    console.log(tutorials)
+
+
+    return(
+        null
+    );
 }
+export default ManageTimetable;
 
