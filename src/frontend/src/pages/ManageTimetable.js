9cb43baa01a74296dad08f5c655c33a0a71c4625
Filip <hey@fmaz.me>
2024-04-29 18:57:15 +1000

    Displays sample course data and can filter based on location

--- src/frontend/src/pages/ManageTimetable.js
+++ src/frontend/src/pages/ManageTimetable.js
@@ -8,43 +8,70 @@ import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid'
 
 function ManageTimetable(props){
+    const [filteredTimetable, setFilteredData] = useState([]);
     const [timetable, setTimetable] = useState([]);
+    const [activeView, setView] = useState(['HN1.23', 'HN1.24', 'N109', 'N111', 'N112', 'N113', 'N114', 'N115/6'])
+
+    const initData = async (t) => {
+        setTimetable(t)
+        filterData(t);
+        
+    }
+
+    const filterData = async (t) => {
+        const filteredData = t.filter((tut) => activeView.includes(tut.location))
+        setFilteredData(filteredData)
+    }
+
+    const toggleView = (room) => {
+        var viewable = activeView;
+        if(viewable.includes(room)){
+            const rem = viewable.indexOf(room);
+            viewable.splice(rem, 1)
+        }else{
+            viewable.push(room)
+        }
+        setView(viewable)
+        filterData(timetable);
+    }
 
     const fetchPost = async () => {
-        setTimetable(await helpers.getRoomTimetables());
+        initData(await helpers.getRoomTimetables());
     }
 
     useEffect(() => {fetchPost();}, [])
 
-
     return(
         <div className='manageTimetable'>
-            <FullCalendar
-            plugins={[ timeGridPlugin ]}
-            initialView="timeGridWeek"
-            weekends={false}
-            slotMinTime={"08:00:00"}
-            slotMaxTime={"21:00:00"}
-            initialDate={"2024-01-01"}
-            dayHeaderFormat={{ weekday: 'short' }}
-            customButtons={
-                { 
-                    HN123: {text: 'HN1.23', click: function() {alert('clicked the custom button!');}},
-                    HN124: {text: 'HN1.24', click: function() {alert('clicked the custom button!');}},
-                    N109: {text: 'N109', click: function() {alert('clicked the custom button!');}},
-                    N111: {text: 'N111', click: function() {alert('clicked the custom button!');}},
-                    N112: {text: 'N112', click: function() {alert('clicked the custom button!');}},
-                    N113: {text: 'N113', click: function() {alert('clicked the custom button!');}},
-                    N114: {text: 'N114', click: function() {alert('clicked the custom button!');}},
-                    N1156: {text: 'N115/6', click: function() {alert('clicked the custom button!');}},
+            <div className='timetable-calendar'>
+                <FullCalendar
+                plugins={[ timeGridPlugin ]}
+                initialView="timeGridWeek"
+                weekends={false}
+                slotMinTime={"08:00:00"}
+                slotMaxTime={"21:00:00"}
+                initialDate={"2024-01-01"}
+                dayHeaderFormat={{ weekday: 'short' }}
+                customButtons={
+                    { 
+                        HN123: {text: 'HN1.23', click: function() { toggleView("HN1.23") }},
+                        HN124: {text: 'HN1.24', click: function() { toggleView("HN1.24") }},
+                        N109: {text: 'N109', click: function() { toggleView("N109") }},
+                        N111: {text: 'N111', click: function() { toggleView("N111") }},
+                        N112: {text: 'N112', click: function() { toggleView("N112") }},
+                        N113: {text: 'N113', click: function() { toggleView("N113") }},
+                        N114: {text: 'N114', click: function() { toggleView("N114") }},
+                        N1156: {text: 'N115/6', click: function() { toggleView("N115/6") }},
+                    }}
+                headerToolbar={{
+                    start: '', // will normally be on the left. if RTL, will be on the right
+                    center: 'HN123 HN124 N109 N111 N112 N113 N114 N1156',
+                    end: '' // will normally be on the right. if RTL, will be on the left
                 }}
-            headerToolbar={{
-                start: '', // will normally be on the left. if RTL, will be on the right
-                center: 'HN123 HN124 N109 N111 N112 N113 N114 N1156',
-                end: '' // will normally be on the right. if RTL, will be on the left
-              }}
-            events={timetable}
-            />
+                events={filteredTimetable}
+                />
+            </div>
+
         </div>
     );
 }
--- src/frontend/src/utils/helperFunctions.js
+++ src/frontend/src/utils/helperFunctions.js
@@ -1,6 +1,8 @@
 import { doc, setDoc, where } from "firebase/firestore";
 import {collection, getDocs, query} from 'firebase/firestore'
 import {database} from '../firebase';
+import data from "../sampletimetable.json"
+
 
 /**
  * Creates class objects from csv file uploads to firebase.
@@ -135,27 +137,100 @@ function dayToNum(day){
   }
 }
 
+
 /**
  * 
  * @returns Object with an array of tutorials in each room organised by their time
  */
 export async function getRoomTimetables(){
-  const timetable = await getData("timetable/"+(await getData("timetable"))[0].id+"/tutorials");
-  const calendarData = [];
-
+  const timetable = await getData("timetable/"+(((await getData("timetable")).sort((a, b) => (a.created.seconds >= b.created.seconds) ? 1 : -1)))[0].id+"/tutorials");
   timetable.forEach(tutorial => {
-    calendarData.push({
-      start: "2024-01-01",
-      end: "2024-30-12",
-      startTime: tutorial.time+":00:00",
-      endTime: (tutorial.time + (tutorial.length / 60)) + ":00:00",
-      daysOfWeek: dayToNum(tutorial.day),
-      title: tutorial.id,
-      editable: true,
-      overlap: true
+    var colour;
+    switch(tutorial.location){
+      case("HN1.23"):
+        colour = "#ea5545";
+        break;
+      case("HN1.24"):
+        colour = "#f46a9b";
+        break;
+      case("HN109"):
+        colour = "#ef9b20";
+        break;
+      case("N111"):
+        colour = "#ede15b";
+        break;
+      case("N112"):
+        colour = "#bdcf32";
+        break;
+      case("N113"):
+        colour = "#87bc45";
+        break;
+      case("N114"):
+        colour = "#27aeef";
+        break;
+      case("N115/6"):
+        colour = "#b33dc6";
+        break;
+    }
+    tutorial.backgroundColor = colour;
+  });
+  console.log(timetable)
+  return timetable;
+}
+
+
+
+
+
+
+
+// ------------------------------------------------
+// DONT CALL BELOW FOR ANYTHING - THIS IS ONLY USED TO UPLOAD SAMPLE DATA TO FIREBASE
+//
+export function uploadSampleData(){
+  const sampleData = data;
+  let i = 0;
+  sampleData.rooms.forEach(room => {
+    i = 0
+    room.allocations.forEach(tut => {
+      i++
+      const code = tut.code + "_" + i
+      setDoc(doc(database, "timetable/dev_sample_data/tutorials", code), createClass(room.room.split(' ')[0], tut));
+
+
     })
   });
 
-  console.log(calendarData)
-  return calendarData;
+}
+
+function createClass(room, tutorial){
+  const time = (tutorial.time)
+  var start_time;
+  var end_time;
+  if(["COMP1100", "COMP2120", "COMP4600", "COMP2310"].includes(tutorial.code)){
+    if(time % 2){
+      start_time = Math.floor((tutorial.time/2) + 8) + ":30:00"
+      end_time = Math.floor((tutorial.time/2) + 10) + ":30:00" 
+    }else{
+      start_time = (tutorial.time/2 + 8) + ":00:00"
+      end_time = (tutorial.time/2 + 10) + ":00:00" 
+    }
+  }else{
+    if(time % 2){
+      start_time = Math.floor((tutorial.time/2) + 8) + ":30:00"
+      end_time = Math.floor((tutorial.time/2) + 10) + ":00:00" 
+    }else{
+      start_time = (tutorial.time/2 + 8) + ":00:00"
+      end_time = (tutorial.time/2 + 9) + ":30:00" 
+    }
+  }
+  return({
+    startTime: start_time,
+    endTime: end_time,
+    daysOfWeek: (tutorial.day + 1).toString(),
+    title: tutorial.code,
+    editable: true,
+    overlap: true,
+    location: room,
+  })
 }
