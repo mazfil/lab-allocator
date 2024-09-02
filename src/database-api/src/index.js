a55d7f20ed410aaaac14da84b5818633b145e187
Filip <hey@fmaz.me>
2024-09-02 11:58:35 +1000

    test query to database

--- src/database-api/src/index.js
+++ src/database-api/src/index.js
@@ -5,9 +5,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const { MongoClient } = require('mongodb');
+const client = new MongoClient('mongodb://localhost:27017');
+await client.connect();
+
 app.get("/api/data/:collection?", (req, res) => {
     const collection = req.params.collection;
 
+    var db = client.db("laballocator")
+    res.json(db.collection("course_data").find({}));
+/*
     switch (collection){
       case "course-data":
         res.json({ message: "Course Data"});
@@ -17,7 +24,7 @@ app.get("/api/data/:collection?", (req, res) => {
         break;
       default:
         res.json({ message: "No Collection Specified."});
-    }
+    }*/
   });
 
 
