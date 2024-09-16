9d7f85056584511937aa526be56509e2ff008831
Filip Mazur <110886132+mazfil@users.noreply.github.com>
2024-09-15 19:23:01 -0700

    Update index.js

--- src/database-api/src/index.js
+++ src/database-api/src/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
